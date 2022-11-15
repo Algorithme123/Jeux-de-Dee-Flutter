b57db242ab351952fdd6baac7c7ba4a517ef7fb2
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-11-15 19:55:32 +0000

    Jeux de Dee implentation du ramdom sur le changement des images du Dee

---
+++ android/app/src/main/kotlin/com/example/projetdedee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projetdedee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int deeGauche =1;
+  int deeDroit =1;
+  
+  void rechargerDee(){
+    setState((){
+      deeGauche = Random().nextInt(6) +1;
+      deeDroit = Random().nextInt(6) +1;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Text("Jeux de dee Daniel",
+          style:  TextStyle(
+            fontSize: 15.0,
+          ),),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Expanded(
+                flex: 1,
+                child: TextButton(
+                  onPressed: (){
+                    print("Dee de Gauche");
+                    print(deeGauche);
+                    rechargerDee();
+                  },
+                  child: Image(
+                    image: AssetImage('images/dice$deeGauche.png'),
+                  ),
+                ),
+
+              ),
+              Expanded(
+                flex: 1,
+                child: TextButton(
+                  onPressed: (){
+                    print("Dee de droite");
+                    print(deeDroit);
+                    rechargerDee();
+                  },
+                  child: Image(
+                    image: AssetImage('images/dice$deeDroit.png'),
+                  ),
+                ),
+
+              ),
+
+
+
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
